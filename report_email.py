8f106fa9797c0fd8150b35f1fbca6e0529897d67
Oscar <osvaldo15963@gmail.com>
2020-08-01 14:56:36 -0400

    finish report_email.py

--- report_email.py
+++ report_email.py
@@ -4,17 +4,16 @@ import datetime
 import os
 import run
 
+time = datetime.datetime.now()
+title = "Processed Update on "+time.strftime("%B")+" "+str(time.day)+", "+str(time.year)
+
 files = run.get_files()
 add_info = ''
 for text in files:
     txt = run.PATH+'\\'+text
     with open(txt, 'r') as txt:
-        add_info += 'name: '+txt.readline()
-        add_info += 'weight: '+txt.readline()
-    add_info += '\n'
-print(add_info)
+        add_info += 'name: '+txt.readline()+'<br/>'
+        add_info += 'weight: '+txt.readline()+'<br/>'
+    add_info += '<br/> <br/>'
 
-"""
-time = datetime.datetime.now()
-title = "Processed Update on "+time.strftime("%B")+" "+str(time.day)+", "+str(time.year)
-"""
+reports.generate_report("C:\\Users\\OSCAR\\Desktop\\Processed.pdf", title, add_info)
