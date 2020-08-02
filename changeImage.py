1d371889e0e68d919587ac1f133e1308c8404524
student-00-2f21e74338af <student-00-2f21e74338af@example.com>
2020-08-02 18:39:57 +0000

    dos2unix all files

--- changeImage.py
+++ changeImage.py
@@ -5,15 +5,15 @@ from PIL import Image
 
 def get_images():
     files = []
-    for (root, path, filename) in os.walk('/home/student-00-b40cf9a1ca5d/images'):
+    for (root, path, filename) in os.walk('/home/student-00-2f21e74338af/supplier-data/images'):
         files.extend(filename)
     return files
 
-
 images = get_images()
+
 for img in images:
-    ini = '/home/student-00-b40cf9a1ca5d/images/'+img
-    out = '/opt/icons/'+img
+    ini = '/home/student-00-2f21e74338af/supplier-data/images/'+img
+    out = '/home/student-00-2f21e74338af/supplier-data/images/'+img.strip('.tiff')+'.jpeg'
     with Image.open(ini) as im:
         newim = im.convert('RGB').resize((600, 400))
         newim.save(out, 'jpeg')
