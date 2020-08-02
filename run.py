6e4a0b39c52838088e8fea0dfe5fb306657f5ce6
student-00-2f21e74338af <student-00-2f21e74338af@example.com>
2020-08-02 19:51:03 +0000

    Finished

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import os
 import requests
+import json
 
-PATH = 'C:\\Users\\OSCAR\\Desktop\\PythonIT\\final_projects\\module4\\Data'
-
+PATH = '/home/student-00-2f21e74338af/supplier-data/descriptions'
+PATHIMG = '/home/student-00-2f21e74338af/supplier-data/images'
 
 def get_files():
     texts = []
@@ -14,30 +15,34 @@ def get_files():
     return texts
 
 
-def dic_maker(txt):
-    path = PATH+'\\'+txt
+def dic_maker(txt, images, image_number):
+
+    path = PATH+'/'+txt
     dictt = {}
     with open(path) as text:
         lines = text.readlines()
         dictt['name'] = lines[0].strip()
-        dictt['weight'] = lines[1].strip()
+        dictt['weight'] = int(lines[1][:3].strip())
         dictt['description'] = lines[2].strip()
+        dictt['image_name'] = images[image_number]
+
+
     return dictt
 
 
 def main():
+    image_number = 0
     files = get_files()
-    print(files)
-    for txt in files:
-        p = dic_maker(txt)
-        print(p)
+    images = [item for item in os.listdir(PATHIMG)]
+    files.sort()
+    images.sort()
 
     for txt in files:
-        p = dic_maker(txt)
-        response = requests.post("http://34.66.47.98/feedback", data=p)
+        p = dic_maker(txt, images, image_number)
+        response = requests.post("http://34.72.254.112/fruits/", json=p)
         print(response.status_code)
+        image_number+=1
         p = {}
-
 
 if __name__ == "__main__":
     main()
